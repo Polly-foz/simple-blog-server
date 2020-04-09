673d255f3e2b7ac76de27a156019de5375b2b5b7
polly-foz <polly.foz@gmail.com>
2020-04-09 20:21:55 +0800

    del blog, login: router and controller

---
+++ src/controller/user.js
@@ -0,0 +1,11 @@
+// src/controller/user.js
+const loginCheck = (username, password) => {
+    // 先使用假数据
+    if (username === 'zhangsan' && password === '123') {
+        return true;
+    }
+    return false;
+};
+module.exports = {
+    loginCheck
+};
--- src/router/user.js
+++ src/router/user.js
@@ -1,12 +1,18 @@
 // src/router/user.js
+const {SuccessModel,ErrorModel} = require('../model/resModel')
+const {loginCheck} = require('../controller/user');
 const handleUserRouter = (req, res) => {
     const method = req.method; // GET POST
 
     // 登录
     if (method === 'POST' && req.path === '/api/user/login') {
-        return {
-            msg: '这是登录的接口'
-        };
+        const {username, password} = req.body;
+        const result = loginCheck(username, password);
+        if (result) {
+            return new SuccessModel();
+        } else {
+            return new ErrorModel('登录失败');
+        }
     }
 
 };
