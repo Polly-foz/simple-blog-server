c1f31cf7d4c567a69a9665adbd5d99a92be5a009
polly-foz <polly.foz@gmail.com>
2020-04-09 16:13:13 +0800

    blog detail: router and controller

--- src/router/blog.js
+++ src/router/blog.js
@@ -1,6 +1,6 @@
 // src/router/blog.js
 const {SuccessModel,ErrorModel} = require('../model/resModel')
-const {getList} = require('../controller/blog')
+const {getList,getDetail} = require('../controller/blog')
 const handleBlogRouter = (req, res) => {
     const method = req.method; // GET POST
 
@@ -14,9 +14,9 @@ const handleBlogRouter = (req, res) => {
 
     // 获取博客详情
     if (method === 'GET' && req.path === '/api/blog/detail') {
-        return {
-            msg: '这是获取博客详情的接口'
-        };
+        const id = req.query.id
+        const data = getDetail(id)
+        return new SuccessModel(data)
     }
 
     // 新建一篇博客
